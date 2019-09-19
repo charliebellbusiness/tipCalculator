40fb8ac2c7b571dba093966df4c2a624cbb71181
Charles Bell <charlesbell@MacBook-Pro-23.local>
2019-09-19 10:54:35 -0500

    Added UI with framework for functionality

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+//Function activated on button press
+//We're passing an anonymous function (a function without a name to the button press)
+
+document.getElementById("calculateTip").onClick = function() {
+    console.log("Button was clicked");
+}
